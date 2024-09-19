1de6c846752edeab80792550d1c8788200362607
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-09-19 23:02:38 +0600

    added recipe category filter sidebar

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,16 +5,19 @@ const API_ID = "3ccee09e";
 const API_KEY = "302b7c800b12ffab2be3b34bed08acf6";
 const TYPE = "public";
 
-export  const fetchData = async function(queries, successCallback){
-    const query = queries?.join("&")
-    .replace(/,/g,"=")
+export const fetchData = async function (queries, successCallback) {
+  const query = queries
+    ?.join("&")
+    .replace(/,/g, "=")
     .replace(/ /g, "%20")
     .replace(/\+/g, "%2B");
-    const url = `${ACCESS_POINT}?app_id=${API_ID}&app_key=${API_KEY}&type=${TYPE}&${query ? `${query}` : ""}`;
-    const response = await fetch(url);
-    if(response.ok){
-        const data = await response.json();
-        successCallback(data)
-        console.log(data)
-    }
-}
+  const url = `${ACCESS_POINT}?app_id=${API_ID}&app_key=${API_KEY}&type=${TYPE}&${
+    query ? `${query}` : ""
+  }`;
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+    console.log(data);
+  }
+};
--- assets/js/global.js
+++ assets/js/global.js
@@ -8,14 +8,14 @@ window.addEventOnElements = (elements, eventType, callback) => {
   }
 };
 
-const cardQueries = [
+export const cardQueries = [
   ["field", "uri"],
   ["field", "label"],
   ["field", "image"],
   ["field", "totalTime"],
 ];
 
-const skeletonCard = `
+export const skeletonCard = `
  <div class="card skeleton-card">
             <div class="skeleton card-banner"></div>
               <div class="card-body">
@@ -38,13 +38,31 @@ window.onSaveRecipe = function (element, recipeId) {
       );
       element.classList.toggle("saved");
       element.classList.toggle("removed");
+      showNotification("Added to Recipe book")
     });
     ACCESS_POINT = ROOT;
   } else {
     window.localStorage.removeItem(`cookio-recipe${recipeId}`);
     element.classList.toggle("saved");
     element.classList.toggle("removed");
+    showNotification("Remove from Recipe book")
   }
 };
 
-export { cardQueries, skeletonCard };
+
+const snackbarContainer = document.createElement("div");
+snackbarContainer.classList.add("snackbar-container");
+document.body.appendChild(snackbarContainer);
+
+
+function showNotification(message){
+  const snackbar = document.createElement("div"); 
+  snackbar.classList.add("snackbar");
+  snackbar.innerHTML = ` <p class="body-medium">${message}/p>`
+  snackbarContainer.appendChild(snackbar);
+  snackbar.addEventListener("animationend", e=>{
+    snackbarContainer.removeChild(snackbar)
+  })
+}
+
+
